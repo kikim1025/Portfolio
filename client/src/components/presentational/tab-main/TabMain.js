33f9bedc00481cd3a308206685988380522268d1
Ki <kikim1025@gmail.com>
2019-06-17 20:44:18 -0400

    added main tab styling

--- client/src/components/presentational/tab-main/TabMain.js
+++ client/src/components/presentational/tab-main/TabMain.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './TabMain.css';
 
+// This tab simply uses SVG assets to display the main graphic through CSS
 const TabMain = () => (
     <div id='tab-main'>
-        
+        <div className='circle background'></div>
+        <div className='circle outer'></div>
+        <div className='circle middle'></div>
+        <div className='circle inner'></div>
+        <div className='circle name'></div>
     </div>
 );
 
