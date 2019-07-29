2816f5f65fb428a64b881bed47a8b136ba9b6c2c
Ki <kikim1025@gmail.com>
2019-07-29 04:50:48 -0400

    test

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Page from './components/presentational/Page';
 
 const App = () => {
+    console.log('test');
     return (
         <div id='App'>
             <Page />
